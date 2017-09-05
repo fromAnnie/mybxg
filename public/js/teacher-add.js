358a63843cf2c1e659c35cf60a4614fa857f6da7
fromAnnie <1679702248@qq.com>
2017-09-05 17:18:50 +0800

    添加表单验证插件

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','datepicker','language'],function($,template,util){
+define(['jquery','template','util','datepicker','language','validate'],function($,template,util){
 	// 设置导航菜单选中
 	util.setMenu('/teacher/list');
 	// 获取编辑讲师的ID
@@ -30,22 +30,50 @@ define(['jquery','template','util','datepicker','language'],function($,template,
 		// 绑定添加的提交事件
 		submitForm('/api/teacher/add');
 	}
+
 	// 实现表单提交功能
 	function submitForm(url){
-		$('#formBtn').click(function(){
-				$.ajax({
-			type : 'post',
-			url : url,
-			data : $('#formId').serialize(),
-			dataType : 'json',
-			success : function(data){
-				if(data.code == 200){
-					location.href = '/teacher/list';
+		$('#formId').validate({
+			sendForm : false,   //阻止表单的默认提交
+			valid : function(){
+				console.log(123);
+			},
+			// 定制属性
+			description : {
+				tc_name : {
+					required : '用户名不能为空',
+					valid : '用户名可以使用'
+				},
+				tc_pass : {
+					required : '密码不能为空',
+					pattern : '必须是6位数字',
+					valid : '密码有效'
+				},
+				tc_join_date : {
+					required : '入职日期不能为空',
+					valid : '日期有效'
 				}
 			}
-			});
 		});
-	
 	}
+
+
+	// 实现表单提交功能  之前的做法
+	// function submitForm(url){
+	// 	$('#formBtn').click(function(){
+	// 			$.ajax({
+	// 		type : 'post',
+	// 		url : url,
+	// 		data : $('#formId').serialize(),
+	// 		dataType : 'json',
+	// 		success : function(data){
+	// 			if(data.code == 200){
+	// 				location.href = '/teacher/list';
+	// 			}
+	// 		}
+	// 		});
+	// 	});
+	
+	// }
 
 });
